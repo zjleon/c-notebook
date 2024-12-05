f43f2d8d82ad5ecc7adef9eb3bef128e1b1aaf2e
jianzhang <jianzhang@caih.com>
2024-12-05 16:14:01 +0800

    add chapter 4 exercise

---
+++ C4/string.c
@@ -0,0 +1,14 @@
+#define UPRINTF_IMPLEMENTATION
+#include "../libs/uprintf.h"
+#define ElemType char
+#include <string.h>
+
+struct String {
+    char chars;
+    int length;
+};
+
+int StrAssign(struct String *str, char chars) {
+    
+    return 0;
+};
